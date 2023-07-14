3736dee700881b82f4737e29ec7034c6f6b9716d
Emil CHigu <emil.c@bluefusesystems.com>
2023-07-14 14:28:58 +0100

    fix: Correct to return base64instead of buffer

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ async function imgUrlToBase64(url) {
   const response = await axios.get(url,  { responseType: 'arraybuffer' })
   const buffer = Buffer.from(response.data, "utf-8")
   
-  return buffer;
+  return buffer.toString('base64');
 }
 
 export default imgUrlToBase64;
